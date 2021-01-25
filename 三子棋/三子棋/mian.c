74137ead4e6557fc9144c7c797027920ab2e41cc
Sugarylife <953882438@qq.com>
2021-01-25 11:32:39 +0800

    三子棋

---
+++ 三子棋/三子棋/mian.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+void menu()
+{
+	printf("*********************\n");
+	printf("****   1.PLAY   *****\n");
+	printf("****   0.EXIT   *****\n");
+	printf("*********************\n");
+
+}
+void game()
+{
+	char ret;
+	char board[ROW][COL];
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	while (1)
+	{
+		PlayerMove(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		DisplayBoard(board, ROW, COL);
+		ComputerMove(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		DisplayBoard(board, ROW, COL);
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢！\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢！\n");
+	}
+	else
+	{
+		printf("平局！\n");
+	}
+
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新输入\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
